64bc1432eace85bf679640e629da520b917a7864
amankh <amankhancompq2002@gmail.com>
2023-10-12 21:26:53 +0530

    added the chatbot feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import Game from "./components/Game/Game";
-import Constants from "./utils/constants";
+import React, {useState} from "react";
+// import Game from "./components/Game/Game";
+// import Constants from "./utils/constants";
 import GameZone from './components/GameZone/GameZone';
+import ChatBt from "./components/Chatbot/Chat";
+
 import './App.css';
 
+
+
 const App = () => {
+  // const [showModal, setShowModal] = useState(false);
+  //   const handleOpen = () => {
+  //     setShowModal(true);
+  //   };
+  
+  //   const handleClose = () => {
+  //     setShowModal(false);
+  //   };
+
   return (
     <>
       <div className="quiz-container">
@@ -17,6 +30,9 @@ const App = () => {
       <div className="bg-cyan-950">
         {/* GameZone */}
         <GameZone />
+      </div>
+      <div>
+        <ChatBt/>
       </div>
     </>
   );
